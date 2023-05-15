49547676ed6921381a7ced7fc70c34e1dab5dbcf
MayaDenawa <dengenkala@gmail.com>
2023-05-15 14:14:50 +0700

    register, header

--- component/header.php
+++ component/header.php
@@ -9,6 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
     <title><?= ucwords(substr(basename($_SERVER['PHP_SELF']), 0, -4)); ?></title>
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
 
 </head>
 
