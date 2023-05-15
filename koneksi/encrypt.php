d996547d1f7cdd02835531266128b7c6ba4343c7
atheo810 <atheos810@gmail.com>
2023-05-15 13:12:51 +0800

    feat: auth

---
+++ koneksi/encrypt.php
@@ -0,0 +1,14 @@
+<?php
+interface encrypt
+{
+    public function psrEncrypt($data);
+}
+
+class passwordEncrypt implements encrypt
+{
+    public function psrEncrypt($data)
+    {
+        $generateEncrypt = hash($data, PASSWORD_BCRYPT);
+        return $generateEncrypt;
+    }
+}
