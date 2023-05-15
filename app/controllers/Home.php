33cffad1c071f09314bff4012e721eb9d5b61e31
atheo810 <atheos810@gmail.com>
2023-05-15 23:47:22 +0800

    feat: MVC

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,9 +1,9 @@
 <?php
 
-class Home
+class Home extends Controller
 {
     public function index()
     {
-        echo 'home/index';
+        $this->view('home/index');
     }
 }
--- app/views/component/header.php
+++ app/views/component/header.php
@@ -1,5 +1,5 @@
-<?php include 'koneksi/koneksi.php'; ?>
-<?php include 'koneksi/database.php'; ?>
+<!-- <?php include 'koneksi/koneksi.php'; ?>
+<?php include 'koneksi/database.php'; ?> -->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <title><?= ucwords(substr(basename($_SERVER['PHP_SELF']), 0, -4)); ?></title>
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
 
