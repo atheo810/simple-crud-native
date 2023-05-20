62b02285f731939447500709b923aef343bc0f3c
atheo810 <atheos810@gmail.com>
2023-05-20 14:36:16 +0800

    feat: mahasiswa

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -1,11 +1,13 @@
 <?php
 class Mahasiswa extends Controller
 {
-    public function index($judul = "Mahasiswa"){
+    public function index($judul = "Mahasiswa")
+    {
         $data['judul'] = $judul;
+        $data['mahasiswa'] = $this->getModel('Mahasiswa_model')->getAllMahasiswa();
         $this->view('component/header', $data);
         $this->view('component/navbar');
-        $this->view('mahasiswa/index');
+        $this->view('mahasiswa/index', $data);
         $this->view('component/footer');
     }
 }
--- app/views/component/navbar.php
+++ app/views/component/navbar.php
@@ -12,6 +12,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="<?= BASE_URL; ?>about">About</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= BASE_URL; ?>mahasiswa">Mahasiswa</a>
+                </li>
             </ul>
         </div>
     </div>
